d32131eaebf8862ec6cb30f4e7862f8eb406c09f
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-11 12:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const txt = document.getElementById("item-name-input");
+const num = document.getElementById("item-price-input");
+const btn = document.getElementById("add");
+const itemList = document.getElementById('list');
+const total = document.getElementById('total');
+let grandTotal = 0;
+btn.addEventListener('click',()=>{
+
+  const name = txt.value.trim();
+   const price = parseFloat(num.value.trim());
+   if(name && price){
+    const newRow = itemList.insertRow();
+          const itemCell = newRow.insertCell();
+          const priceCell = newRow.insertCell();
+          itemCell.textContent = name;
+          priceCell.textContent = price.toFixed(2);
+          grandTotal += price;
+          total.textContent = grandTotal.toFixed(2);
+          txt.value = '';
+        num.value = '';
+   }
+
+})
